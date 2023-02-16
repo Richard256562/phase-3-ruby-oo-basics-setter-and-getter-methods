2d872da003850fd166e19e8bf4f71e3fda0839e3
RichardOkongo <rirachard19@gmail.com>
2023-02-16 12:57:03 +0300

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    #Setter Method
+    def name=(name)
+        @name = name
+    end
 
+    #Getter Method
+    def name
+        @name
+    end
+
+    #Setter Method
+    def job=(job)
+        @job = job
+    end
+
+    #Getter Method
+    def job
+        @job
+    end
 end
+
